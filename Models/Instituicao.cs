9868e8e23f14a49762338b356627e9b7671698b6
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-10-17 20:03:26 -0300

    nao sei o que mudou

--- Models/Instituicao.cs
+++ Models/Instituicao.cs
@@ -4,8 +4,8 @@ namespace GestaoEscolar.Models;
 
 public class Instituicao
 {
-    [Key]
-    public long? InstituicaoId { get; set; }
-    public string Nome { get; set; }
-    public string Endereco { get; set; }
+  [Key]
+  public long? InstituicaoId { get; set; }
+  public string Nome { get; set; }
+  public string Endereco { get; set; }
 }

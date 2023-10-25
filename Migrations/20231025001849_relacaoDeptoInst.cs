5d4d3bf6100d9833f541457be0c93bd130041b70
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-10-24 21:25:11 -0300

    migrations do banco de dados criados

---
+++ Migrations/20231025001849_relacaoDeptoInst.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoEscolar.Migrations
+{
+    /// <inheritdoc />
+    public partial class relacaoDeptoInst : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DepartamentoID",
+                table: "Departamentos",
+                newName: "DepartamentoId");
+
+            migrationBuilder.AddColumn<long>(
+                name: "fk_InstituicaoId",
+                table: "Departamentos",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departamentos_fk_InstituicaoId",
+                table: "Departamentos",
+                column: "fk_InstituicaoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departamentos_Instituicoes_fk_InstituicaoId",
+                table: "Departamentos",
+                column: "fk_InstituicaoId",
+                principalTable: "Instituicoes",
+                principalColumn: "InstituicaoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departamentos_Instituicoes_fk_InstituicaoId",
+                table: "Departamentos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departamentos_fk_InstituicaoId",
+                table: "Departamentos");
+
+            migrationBuilder.DropColumn(
+                name: "fk_InstituicaoId",
+                table: "Departamentos");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartamentoId",
+                table: "Departamentos",
+                newName: "DepartamentoID");
+        }
+    }
+}
